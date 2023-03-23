1ce9d66647d0672ac4ce7b7ca039f5e9b699960e
Midland707 <rus14job@ukr.net>
2023-03-24 00:41:21 +0200

    add Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,45 @@
-const { useParams } = require('react-router-dom');
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 const Reviews = () => {
+  const [reviewsCard, setReviewsCard] = useState(null);
   const { movieId } = useParams();
-  return <div>Reviews {movieId}</div>;
+
+  useEffect(() => {
+    runQuery();
+  }, []);
+  // https://api.themoviedb.org/3/movie/272/reviews?api_key=6746b4dbb69b720741ecbdc7655d3557
+  const runQuery = () => {
+    if (movieId) {
+      axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+      const apiKey = '6746b4dbb69b720741ecbdc7655d3557';
+      const typeQuery = 'movie';
+      const subType = 'reviews';
+      axios
+        .get(`${typeQuery}/${movieId}/${subType}?api_key=${apiKey}`)
+        .then(res => {
+          const dataArr = res.data;
+          setReviewsCard(dataArr.results);
+        });
+    }
+  };
+
+  const imgURL = 'https://image.tmdb.org/t/p/original';
+  return (
+    <div>
+      {reviewsCard && (
+        <ul>
+          {reviewsCard.map(review => (
+            <li key={review.id}>
+              <span>Autor{review.author}</span>
+              <span>Character {review.content}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Reviews;
